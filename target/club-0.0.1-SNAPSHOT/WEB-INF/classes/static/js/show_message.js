5e1f79c1f79246d42a4121b2aff77f8fcf3467bd
chichi <j43343@gmail.com>
2019-11-23 23:22:25 +0800

    modify

--- target/club-0.0.1-SNAPSHOT/WEB-INF/classes/static/js/show_message.js
+++
@@ -1,6 +0,0 @@
-	window.onload = function () {
-			if(document.getElementById("show_message")!=null){
-				document.getElementById("show_message").click();
-				document.getElementById("show_message").style.display="none";
-			}
-		}
